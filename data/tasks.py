1d1565e988409aa1813bbf066a9f458f4d5106c7
Tom Redmond <redmondinho@gmail.com>
2019-12-05 11:02:36 +0000

    Remove default task creation

--- data/tasks.py
+++ data/tasks.py
@@ -18,4 +18,3 @@ class Task(BaseModelTasks):
 def init_db():
 	tasks_db.connect()
 	tasks_db.create_tables([Task])
-	task_record = Task.create(user_id=1, title='My Test Tasks', description='This is the first task and it was created automatically')
